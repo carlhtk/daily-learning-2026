d77f8f55eeb1359480c305754ec5c33689c66c94
sandoval <juansandoval.cv@gmail.com>
2026-03-30 15:26:45 -0600

    Dia 2: Entorno configurando y venv ignorado

--- src/app.py
+++ src/app.py
@@ -1,3 +1,21 @@
-import sys
-print(f"Environment: {sys.prefix}")
-print("Everything is running perfectly!")
+import os 
+from datetime import datetime 
+
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+def main():
+
+    user = os.getenv("user_name", "ingeniero")
+    now = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    print(f"--- SISTEMA DE CONTROL ---")
+    print(f"Bienvenido, {user}")
+    print(f"Estado: Conectado")
+    print(f"Fecha/Hora: {now}")
+    print(f"--------------------------")
+
+if __name__ == "__main__":
+    main()
+    
